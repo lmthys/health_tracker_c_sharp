1eaec9cf76d76fcfa820b838d1decc5a46b5690c
lmandrew <lmandrew@iastate.edu>
2018-11-06 10:19:37 -0600

    Adding tests

---
+++ WpfApp1/IsScreenNotNull.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit;
+
+namespace MainScreen.UnitTests.Services
+{
+    public class IsScreenNotNull
+    {
+        public IsScreenNotNull()
+        {
+            MainScreen m = new MainScreen("Test");
+            Assert.True(m != null, "Main screen should not be null");
+        }
+    }
+}
+
